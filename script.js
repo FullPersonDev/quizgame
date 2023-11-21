77178ed1f5c1333c0f06d07bb7805714412daa24
Martin Aguero <martin.aguero84@gmail.com>
2023-11-20 18:13:12 -0600

    added timer to html, added styles to the page with css, added variables and startgame function to javascript

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+//Create variables to reference index.html elements
+var start = document.getElementById('start');
+var time = document.getElementById('time');
+
 //Create variables to capture user input
 
-//Create functions to handle user input
+//Create functions to start game and handle user input
+
+function startGame() {
+    console.log('Start the game already!');
+}
 
 //Create functions to provide results from user input
 
 //Create event listeners
+start.addEventListener('click', startGame);
