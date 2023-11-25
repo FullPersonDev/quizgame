7a9689735cf7c86f656986a24d7e05f8660318b6
Martin Aguero <martin.aguero84@gmail.com>
2023-11-25 10:27:06 -0600

    added the object variable that will contain the questions and its corresponding correctAnswer, also made the center section of page disappear and for the 1st question to appear upon starting the quiz game

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 //Create variables to reference index.html elements
 var start = document.getElementById('start');
 var time = document.getElementById('time');
+var mainSection = document.getElementById('main');
+var landingSection = document.getElementById('landingSection');
+var questionSection = document.getElementById('questionSection');
+var answerSection = document.getElementById('answerSection');
 
 //Create variables to capture user input
 
-//Create functions to start game and handle user input
+//Create the variable object of the questions and its correct answer:
+var questions = [
+    {question1: 'What is 2 + 2',
+    correctAnswer: '4',
+    },
+    {question2: 'What is 4 + 4',
+    correctAnswer: '8',
+    },
+    {question3: 'What is 5 + 5',
+    correctAnswer: '10',
+    },
+];
 
-var timeLeft = 5;
+
+//Create functions to start game and handle user input
+var timeLeft = 80;
 
 function startGame() {
     function countDown() {
@@ -16,12 +33,14 @@ function startGame() {
 
             if (timeLeft === 0) {
                 clearInterval(timeInterval);
-                console.log('sorry all done!');
             }
         }, 1000)
     }
-    console.log('Start the game already!');
     countDown();
+    //Append to the webpage to hide center section and display my questions.
+    landingSection.setAttribute('style', 'display: none;');
+    questionSection.setAttribute('style', 'display: block;');
+    questionSection.textContent = questions[0].question1;
 }
 
 //Create functions to provide results from user input
