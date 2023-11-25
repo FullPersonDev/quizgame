afc238fa14c0342fe7895487e5039f2c836c0b4e
Martin Aguero <martin.aguero84@gmail.com>
2023-11-24 20:47:17 -0600

    added the timer function with setinterval and clearinterval, and working well

--- script.js
+++ script.js
@@ -6,8 +6,22 @@ var time = document.getElementById('time');
 
 //Create functions to start game and handle user input
 
+var timeLeft = 5;
+
 function startGame() {
+    function countDown() {
+        var timeInterval = setInterval(function() {
+            timeLeft--;
+            time.textContent = timeLeft;
+
+            if (timeLeft === 0) {
+                clearInterval(timeInterval);
+                console.log('sorry all done!');
+            }
+        }, 1000)
+    }
     console.log('Start the game already!');
+    countDown();
 }
 
 //Create functions to provide results from user input
