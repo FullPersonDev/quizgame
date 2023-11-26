ed0d460143a76d0bdd248e95cd24b6e2a3d05b72
Martin Aguero <martin.aguero84@gmail.com>
2023-11-26 12:56:55 -0600

    built on the main gameStart function by adding in the internal functions that will help run the game, added the setup and display next question internal functions, also made the options appear as buttons.

--- script.js
+++ script.js
@@ -10,42 +10,78 @@ var resultSection = document.getElementById('resultSection');
 
 //Create variables to capture user input
 
-//Create the variable object of the questions and its correct answer:
+//Create the variable object for the questions and its correct answer:
 var questions = [
-    {question: 'What is 2 + 2',
-    options: ['4', '6', '7', '1',],
-    correctAnswer: '4',
+    {question: 'The condition of the If statement is enclosed in _____.',
+    options: ['square brackets', 'commas', 'parenthesis', 'curly brackets',],
+    correctAnswer: 'parenthesis',
     },
     {question: 'What is 4 + 4',
+    options: [],
     correctAnswer: '8',
     },
     {question: 'What is 5 + 5',
+    options: [],
     correctAnswer: '10',
     },
+    {question: 'What is 10 + 10',
+    options: [],
+    correctAnswer: '20',
+    }
 ];
 
 
 //Create functions to start game and handle user input
 var timeLeft = 80;
+var currentQuestionIndex = 0;
 
+//Created the main function startGame() and its internal functions that will run the game
 function startGame() {
+    timeLeft = 80;
+    currentQuestionIndex = 0;
+    //Append to the webpage to hide center section and display my questions and options
+    landingSection.setAttribute('style', 'display: none;');
+    gameContainer.setAttribute('style', 'display: block;');
+
+    //Calling out these internal functions that power the startGame() main function
+    countDown();
+    nextQuestion();
+    displayQuestion();
+
+    //Setting up the internal functions that power the startGame() main function
     function countDown() {
         var timeInterval = setInterval(function() {
             timeLeft--;
             time.textContent = timeLeft;
 
-            if (timeLeft === 0) {
+            if (timeLeft === 0 || currentQuestionIndex === questions.length) {
                 clearInterval(timeInterval);
+                endGame();
             }
-        }, 1000)
+        }, 1000);
     }
-    countDown();
-    //Append to the webpage to hide center section and display my questions.
-    landingSection.setAttribute('style', 'display: none;');
-    gameContainer.setAttribute('style', 'display: block;');
-    questionSection.textContent = questions[0].question;
-    answerSection.textContent = questions[0].options;
-    resultSection.textContent = 'Correct!';
+
+    //Set up next question with condition statement to: continue or end game.
+    function nextQuestion() {
+        if (currentQuestionIndex < questions.length) {
+            displayQuestion(questions[currentQuestionIndex]);
+        } else {endGame();}
+    }
+    //Display next question with options as buttons
+    function displayQuestion (questions) {
+        questionSection.textContent = questions.question;
+        questions.options.forEach(
+            function (options, index) {
+                var button = document.createElement('button');
+                button.textContent = options;
+                button.addEventListener('click', function() {
+                    checkAnswer(choice, questions.correctAnswer);
+                });
+                answerSection.appendChild(button);
+            }
+        );
+    }
+
 }
 
 //Create functions to provide results from user input
